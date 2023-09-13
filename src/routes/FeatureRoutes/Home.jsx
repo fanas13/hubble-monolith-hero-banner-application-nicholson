d8f4a9c2df25881af8080dcc7a5168f6a74fec98
Nicholson Galela <nicholsongalela@gmail,com>
2023-09-13 12:41:34 +0200

    add the CATs on the home page

--- src/routes/FeatureRoutes/Home.jsx
+++ src/routes/FeatureRoutes/Home.jsx
@@ -1,20 +1,43 @@
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import styled from 'styled-components';
 
 const Home = (props) => {
     const { className } = props;
+    const StyledButton = styled(Button)`
+    && {
+            border-radius: 30px;
+            padding: 10px;
+            font-size: 16px;
+            color: #ffffff
+        }
+    `;
     return(
-        <Grid container item md={8} justifyContent="center">
-            <Grid item xs={12} md={8}>
-                {/* Content for the first column */}
-                <div className={className}>
-                    <Typography variant="body1" style={{ fontWeight: 'bold' }} color="primary">Monolith</Typography>
-                    <Typography variant="h2" color="secondary">Use AI software to test less and learn more.</Typography>
-                    <Typography variant="p" color="secondary">Spend less time running expensive, repetitive tests and more time learning from your engineering data to predict the exact tests to run.</Typography>
-                </div>
-            </Grid>
-            <Grid item xs={12} md={4} />
+        <Grid container item md={8} >
+            <>
+                <Grid item xs={12} md={8}>
+                    {/* Content for the first column */}
+                    <div className={className}>
+                        <Typography variant="body1" style={{ fontWeight: 'bold' }} color="primary">Monolith</Typography>
+                        <Typography variant="h2" color="secondary">Use AI software to test less and learn more.</Typography>
+                        <Typography variant="p" color="secondary">Spend less time running expensive, repetitive tests and more time learning from your engineering data to predict the exact tests to run.</Typography>
+                    </div>
+                </Grid>
+                <   Grid item xs={12} md={4} />
+            </>
+            <>
+                <Grid item spacing={1} xs={12} md={8} container margin="40px 0 0 0">
+                    {/* Content for the first column */}
+                    <Grid item xs={12} md={4}>
+                        <StyledButton fullWidth variant="contained" disableElevation>Request demo</StyledButton>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <StyledButton fullWidth variant="outlined" disableElevation>Speak to our team</StyledButton>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={4} />
+            </>
         </Grid>
     )
 };
